8a1453b518c51bb568118ad1d1dc1ee73168220b
Jeric <marianovjeric@gmail.com>
2022-12-07 12:54:22 -0800

    finished first part

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+const userListEl = document.querySelector('.user-list')
 
 async function main() {
     const users = await fetch("https://jsonplaceholder.typicode.com/users");
     const usersData = await users.json();
-    const userListEl = document.querySelector('.user-list')
-
-    userListEl.innerHTML = usersData
-    .map(
-        (user) => 
-            `<div class="user-card">
-                <div class="user-card__container">
-                    <h3>${user.name}</h4>
-                        <p><b>Email:</b> ${user.email}</p>
-                        <p><b>Phone:</b> ${user.phone}</p>
-                        <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
-                        ${user.website}
-                        </a></p>
-                </div>
-            </div>`
-  )
-  .join("");
-
+    console.log(usersData)
+    userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
 }
 
 main();
+
+function showUserPosts(id) {
+    window.location.href = 'http://127.0.0.1:5500/user.html'
+    console.log(id)
+}
+
+function userHTML(user) {
+    return   `<div class="user-card" onclick="showUserPosts(${user.id})">
+    <div class="user-card__container">
+        <h3>${user.name}</h4>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
+            ${user.website}
+            </a></p>
+    </div>
+</div>`
+}
+
+//11:35
